4ceb95d9b10eb8618d8d5b5f7c5798beef24dde6
José Fernando González <jfgonzalezm@gmail.com>
2022-09-25 14:32:06 +0200

    [feature] Django Restframework installation
    - djrestframework has been install and configured.
    - Create a view, serializer and url.

--- src/store/views.py
+++ src/store/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+
+from .models import Product
+from .serializers import ProductSerializer
+
 
 # Create your views here.
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
